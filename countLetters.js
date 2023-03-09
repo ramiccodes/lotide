ed5186f4ffd11ac46e88545d2fc27dc138649861
Ramic <ramlag1819@gmail.com>
2023-03-08 17:35:33 -0800

    Initial commit

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+// Input: string
+// Output: Object with keys and values letter and how many time it appeared
+const countLetters = (string) => {
+  let count = {};
+
+  for (const char of string) {
+    if (char === ' ') {
+      continue;
+    }
+    count[char] = count[char] ? (count[char] + 1) : 1;
+  }
+  return count
+}
+
+
+console.log(countLetters('this a robbery'));

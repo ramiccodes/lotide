d01d891374de624b504f1468217edbf416fd4ec2
Ramic <ramlag1819@gmail.com>
2023-03-09 20:21:50 -0800

    Initial commit

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const findKey = (obj, func) => {
+  for (const key in obj) {
+    if (func(obj[key]) !== true) {
+      continue;
+    } else {
+      return key;
+    }
+  }
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));

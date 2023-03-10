5ea3bc016abd1cb284e5a001577d1f4d3374a67f
Ramic <ramlag1819@gmail.com>
2023-03-09 19:27:37 -0800

    Initial commit

---
+++ takeUntil.js
@@ -0,0 +1,19 @@
+const takeUntil = (arr, func) => {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i]) !== true) {
+      newArr.push(arr[i]);
+    } else {
+      return newArr;
+    }
+  }
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);

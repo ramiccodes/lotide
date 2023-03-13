97dd2be3b012e9d2666d08425957d56e682335df
Ramic <ramlag1819@gmail.com>
2023-03-13 15:21:07 -0700

    Refactored to use and practice exporting and importing modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,7 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1 == null || arr2 == null) {
-    return false;
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; ++i) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2) === false ? console.log(`😡😡😡 Assertion Failed ${arr1} !== ${arr2}`) : console.log(`😁😁😁 Assertion Passed ${arr1} === ${arr2}`)
 }
 
-eqArrays([1,2,3],[1,2,3]);
-eqArrays([1,2,3],[1,2,"3"]);
-
-assertArraysEqual([1,2,3],[3,2,1]);
-
+module.exports = assertArraysEqual;

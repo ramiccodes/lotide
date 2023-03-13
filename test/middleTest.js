3e97e902d1190e5d65533c543b8d0127b3119bbf
Ramic <ramlag1819@gmail.com>
2023-03-13 16:43:49 -0700

    Refactored middleTest and tailTest to use Mocha and Chai for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,18 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([22,33,44,55]), [33,44]);
-assertArraysEqual(middle([22,33,44,55]), [33,55]);
-assertArraysEqual(middle([22,33,44]), [33]);
-assertArraysEqual(middle([22,33]), []);
+describe('#middle', () => {
+  it('returns [33,44] when [22,33,44,55]', () => {
+    assert.deepEqual(middle([22,33,44,55]), [33,44]);
+  });
+
+  it ('returns [33] when [22,33,44]', () => {
+    assert.deepEqual(middle([22,33,44]), [33]);
+  })
+
+  it ('returns [] when [22,33]', () => {
+    assert.deepEqual(middle([22,33]), []);
+  })
+})
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 'Labs' );
-assertEqual(tail([]), "" );
+describe('#tail', () => {
+  it('returns Labs for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 'Labs')
+  });
+
+  it('returns "" for ["Yo Yo"]', () => {
+    assert.strictEqual(tail(["Yo Yo"]), '')
+  });
+
+  it('returns "" for []', () => {
+    assert.strictEqual(tail([]), '')
+  });
+})
